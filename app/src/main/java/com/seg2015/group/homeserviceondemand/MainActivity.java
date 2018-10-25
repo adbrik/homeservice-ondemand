25535dd94595e823daa7972210c3c864bb88cb48
sebastiant7575 <31967178+sebastiant7575@users.noreply.github.com>
2018-10-25 18:26:41 -0400

    Connected login screen and welcome screen
    Pressing login now moves from loginScreen to welcomeScreen and the username is displayed on the welcomeScreen: "Welcome (user)!"

--- app/src/main/java/com/seg2015/group/homeserviceondemand/MainActivity.java
+++ app/src/main/java/com/seg2015/group/homeserviceondemand/MainActivity.java
@@ -1,13 +1,45 @@
 package com.seg2015.group.homeserviceondemand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TEXT = "com.seg2015.group.homeserviceondemand.EXTRA_TEXT";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openWelcomeActivity();
+
+            }
+        });
+
+    }
+
+    public void openWelcomeActivity(){
+
+        EditText userName_Field = (EditText) findViewById(R.id.userName_Field);
+
+        String text = userName_Field.getText().toString();
+
+        Intent intent = new Intent(this, WelcomeActivity.class);
+
+        intent.putExtra(EXTRA_TEXT, text);
+
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/seg2015/group/homeserviceondemand/WelcomeActivity.java
+++ app/src/main/java/com/seg2015/group/homeserviceondemand/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.seg2015.group.homeserviceondemand;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+
+        TextView textView_name = (TextView) findViewById(R.id.textView_name);
+
+        textView_name.setText(text + "!");
+
+
     }
 }
